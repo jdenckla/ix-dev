768d7d8929d8a885db2a25e767b6f52d2a58a8b4
John Dencklau <jdenckla@ix-dev.cs.uoregon.edu>
2023-10-04 14:58:18 -0700

    incomplete lab1, debug req

--- 415/Lab1/string_parser.c
+++ 415/Lab1/string_parser.c
@@ -26,19 +26,33 @@ int count_token (char* buf, const char* delim)
 	*	#3. return the number of token (note not number of delimeter)
 	*/
 	// rather than use the character buffer directly, make temporary copies
+	// please see https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/ for referenced code
 	int count = 0;
 	char* token;
 	char* rest = buf;
+
+	// null check b/c while loop isn't giving accurate count yet
+
+	// || (buf[0] == '\0')
+	//return 0;
 	if(strlen(buf) == 0) {
-		return 0;
+		printf("%s\n", "lack of buf");
+		return count;
+		//return 0;
+	//	return 0;
+	} else if (buf[0] == '\0') {
+		printf("%s\n", "null character found");
+		count++;
 	} else {
 		//iterate
-			while ((token = strtok_r(rest, " ", &rest))) {
-				count++;
-				//printf("%s\n", token);
+		while (token = strtok_r(rest, delim, &rest)) {
+			count++;
+			//printf("%s\n", token);
+			// this may only be counting delims - adjust if necessary (null check it) ^ see above about null at last token
 			}
-			return count;
 		}
+		printf("%d\n", count);
+		return count;
 	}
 
 command_line str_filler (char* buf, const char* delim)
@@ -55,6 +69,22 @@ command_line str_filler (char* buf, const char* delim)
 	*			fill command_list array with tokens, and fill last spot with NULL.
 	*	#6. return the variable.
 	*/
+	command_line var;
+	var.num_token = count_token(buf, delim);;
+	char* token;
+	char* rest = buf;
+	int i = 0;
+	//num_token = count_token(buf, delim);
+	var.command_list = malloc(var.num_token * sizeof(char*));
+	//for (int i = 0, i < num_token, i++) {
+	while (token = strtok_r(rest, delim, &rest)) {
+		var.command_list[i] = malloc(strlen(token) * sizeof(char));
+		var.command_list[i] = token;
+		i++;
+	}
+	var.command_list[i] = NULL;
+	return var;
+	//}
 }
 
 
@@ -64,4 +94,9 @@ void free_command_line(command_line* command)
 	/*
 	*	#1.	free the array base num_token
 	*/
+	//for (int i = 0; i <= command->num_token; i++) {
+	//	free(command->command_list[i]);
+	//}
+	free(command->command_list);
+	
 }
