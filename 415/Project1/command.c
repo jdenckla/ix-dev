7ee8e0061ba43749efd5229dbb12e7f99bd9fcea
John Dencklau <jdenckla@ix-dev.cs.uoregon.edu>
2023-10-09 21:18:06 -0700

    Pushing stubbed functionality

--- 415/Project1/command.c
+++ 415/Project1/command.c
@@ -5,18 +5,79 @@
 #include <fcntl.h>  
 #include <string.h>
 
-void listDir(); /*for the ls command*/
+/*for the ls command*/
+void listDir()
+{
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+		//printf("Current working dir: %s\n", cwd);
+		DIR *pDir;
+		pDir = opendir(cwd);
+		struct dirent *dp;
+		while ((dp = readdir(pDir)) != NULL){
+            write(1,dp->d_name,strlen(dp->d_name));
+            write(1,"\n",1);
+    	}
+	closedir(pDir);
+	} 
+	return;
+} 
 
-void showCurrentDir(); /*for the pwd command*/
+/*for the pwd command*/
+void showCurrentDir() 
+{
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        write(1,cwd,strlen(cwd));
+        write(1,"\n",1);
+	} 
+	return;
+} 
 
-void makeDir(char *dirName); /*for the mkdir command*/
+/*for the mkdir command*/
+// create dir in current directory
+void makeDir(char *dirName)
+{
+	printf("%s%s\n","Create ",dirName);
+	return;
+} 
 
-void changeDir(char *dirName); /*for the cd command*/
+/*for the cd command*/
+// find dir (?), then chane current to this
+void changeDir(char *dirName)
+{
+	printf("%s%s\n","Relocate ",dirName);
+	return;
+}  
 
-void copyFile(char *sourcePath, char *destinationPath); /*for the cp command*/
+/*for the cp command*/
+// find file (open/close?) (or err/fail), find dest (?), cp file to dest
+void copyFile(char *sourcePath, char *destinationPath)
+{
+	printf("%s%s%s%s\n","Copy ",sourcePath," to ",destinationPath);
+	return;
+}  
 
-void moveFile(char *sourcePath, char *destinationPath); /*for the mv command*/
+/*for the mv command*/
+// pos perform copyFile command, then deleteFile command
+void moveFile(char *sourcePath, char *destinationPath)
+{
+	printf("%s%s%s%s\n","Move ",sourcePath," to ",destinationPath);
+	return;
+}  
 
-void deleteFile(char *filename); /*for the rm command*/
+/*for the rm command*/
+// find file (open/close?) (or err/fail), delete it
+void deleteFile(char *filename)
+{
+	printf("%s%s\n","Remove ",filename);
+	return;
+}  
 
-void displayFile(char *filename); /*for the cat command*/
+/*for the cat command*/
+// enter a file (or err/fail), read each line (getline), and print to stdout (write(1,buf,buflen))
+void displayFile(char *filename)
+{
+	printf("%s%s\n","Print ",filename);
+	return;
+}  
--- 415/Project1/main.c
+++ 415/Project1/main.c
@@ -11,12 +11,13 @@
 
 int main(int argc, char const *argv[])
 {
-	char userInput[255];
+	
 	// getopt ref: https://www.gnu.org/software/libc/manual/html_node/Example-of-Getopt.html#Example-of-Getopt
+	
+	/*
 	int flag = 0;
 	char *fname = NULL;
 	int opt;
-
 	//opterr = 0;
 
 	while ((opt = getopt (argc, argv, "f:")) != -1) 
@@ -38,19 +39,119 @@ int main(int argc, char const *argv[])
 			fprintf(stderr, "Unknown option/argument");
 		}
 	}
+	*/
 
+	
+	
 	
 	
 	do {
-		printf(">>>:");
-		scanf("%s", userInput);
+		size_t size = 128;
+		char *userInput = malloc (size);
+		ssize_t chars_read;
 
+		//size_t len = 128;
+		//char* line_buf = malloc (len);
+
+		command_line large_token_buffer;
+		command_line small_token_buffer;
+		printf(">>>:");
+		// pos have file pass to stdin, or swap stdin stream for variable set by flag
+		chars_read = getline(&userInput, &size, stdin);
+		userInput[strcspn(userInput, "\r\n")] = 0;
+		if (chars_read < 0){
+			puts("Err - no input. Exiting.");
+			free(userInput);
+			return 1;
+		}
+		//scanf("%s", userInput);
+		large_token_buffer = str_filler (userInput, ";");
+		for (int i = 0; large_token_buffer.command_list[i] != NULL; i++)
+		{
+			//printf ("\tLine segment %d:\n", i + 1);
+			//tokenize large buffer
+			//smaller token is seperated by " "(space bar)
+			small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
+			//iterate through each smaller token to print
+
+			if (strcmp("exit",small_token_buffer.command_list[0]) == 0) {
+				return 0;
+			} else if (strcmp("ls",small_token_buffer.command_list[0]) == 0) {
+				if (small_token_buffer.command_list[1] != NULL) {
+					printf("%s\n", "Error - ls takes no additional parameters.");
+				} else {
+					listDir();
+				}
+			} else if (strcmp("pwd",small_token_buffer.command_list[0]) == 0) {
+				if (small_token_buffer.command_list[1] != NULL) {
+					printf("%s\n", "Error - pwd takes no additional parameters.");
+				} else {
+					showCurrentDir();
+				}
+			} else if (strcmp("mkdir",small_token_buffer.command_list[0]) == 0) {
+				//makeDir();
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] != NULL)) {
+					printf("%s\n", "Error - mkdir takes one additional parameter.");
+				} else {
+					makeDir(small_token_buffer.command_list[1]);
+				}
+			} else if (strcmp("cd",small_token_buffer.command_list[0]) == 0) {
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] != NULL)) {
+					printf("%s\n", "Error - cd takes one additional parameter.");
+				} else {
+					changeDir(small_token_buffer.command_list[1]);
+				}
+			} else if (strcmp("cp",small_token_buffer.command_list[0]) == 0) {
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] == NULL) || (small_token_buffer.command_list[3] != NULL)) {
+					printf("%s\n", "Error - cp takes two additional parameters.");
+				} else {
+					copyFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+				}
+			} else if (strcmp("mv",small_token_buffer.command_list[0]) == 0) {
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] == NULL) || (small_token_buffer.command_list[3] != NULL)) {
+					printf("%s\n", "Error - mv takes two additional parameters.");
+				} else {
+					moveFile(small_token_buffer.command_list[1], small_token_buffer.command_list[2]);
+				}
+			} else if (strcmp("rm",small_token_buffer.command_list[0]) == 0) {
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] != NULL)) {
+					printf("%s\n", "Error - rm takes one additional parameter.");
+				} else {
+					deleteFile(small_token_buffer.command_list[1]);
+				}
+			} else if (strcmp("cat",small_token_buffer.command_list[0]) == 0) {
+				if ((small_token_buffer.command_list[1] == NULL) || (small_token_buffer.command_list[2] != NULL)) {
+					printf("%s\n", "Error - cat takes one additional parameter.");
+				} else {
+					displayFile(small_token_buffer.command_list[1]);
+				}
+			} else {
+				printf("%s\n","Error: Unrecognized command!");
+				// print user input as well
+			}
+
+			//for (int j = 0; small_token_buffer.command_list[j] != NULL; j++)
+			//{
+				//printf ("\t\tToken %d: %s\n", j + 1, small_token_buffer.command_list[j]);
+				// here we can interpret each part of a command (based on spaces)...
+			//}
+
+
+			//free smaller tokens and reset variable
+			free_command_line(&small_token_buffer);
+			memset (&small_token_buffer, 0, 0);
+		}
+		free_command_line (&large_token_buffer);
+		memset (&large_token_buffer, 0, 0);
+		free (userInput);
+
+		/*
 		if (strcmp("exit",userInput) == 0) {
 			break;
 		} else if (strcmp("ls",userInput) == 0) {
-			//listDir();
+			listDir();
 		} else if (strcmp("pwd",userInput) == 0) {
-			//showCurrentDir();
+			showCurrentDir();
 		} else if (strcmp("mkdir",userInput) == 0) {
 			//makeDir();
 		} else if (strcmp("cd",userInput) == 0) {
@@ -67,6 +168,7 @@ int main(int argc, char const *argv[])
 			printf("%s\n","Error: Unrecognized command!");
 			// print user input as well
 		}
+		*/
 	} while(1);
 
 	return 0;
