d997760cc29bbd59eee474fbf3f180097b0caf8a
John Dencklau <jdenckla@ix-dev.cs.uoregon.edu>
2023-11-04 20:59:28 -0700

    ts double free when forking

--- 415/Project2/part1.c
+++ 415/Project2/part1.c
@@ -13,10 +13,8 @@
 int countLines(char *filename)
 {
 // line counter sourced from: https://codereview.stackexchange.com/questions/156477/c-program-to-count-number-of-lines-in-a-file
-	//printf("Begin counting lines\n");
 	int counter = 0;
 	FILE *fp;
-	//char c;
 	fp = fopen(filename,"r");
 	char buffer[SIZE + 1], lastchar = '\n';
 	size_t bytes;
@@ -31,11 +29,10 @@ int countLines(char *filename)
 			}
 		}
 		if (lastchar != '\n') {
-			counter++;  /* Count the last line even if it lacks a newline */
+			counter++; 
 		}
 	}
 	fclose(fp);
-	printf("End counting lines - %d\n",counter);
 	return counter;
 }
 
@@ -43,29 +40,10 @@ int main(int argc, char const *argv[])
 {
 	//printf("Start the program\n");
 	FILE *inFile;
-	FILE *freOp;
 	char *filenameSrc;
-	int flagSet = 0;
-	//char *exitText = "Bye Bye!";
-	char *errCmd = "Error! Unrecognized command: ";
-	char *errParam = "Error! Unrecognized parameters for command: ";
 
-	char *errRedirOut;
-
-	flagSet = 1;
-	//printf("Grab the filename\n");
 	filenameSrc = strdup(argv[1]);
 	int numLines = countLines(filenameSrc);
-	/*
-	freOp = freopen("output.txt","w",stdout);
-	if (freOp == NULL) {
-		char *errRedirOut = "Error! Failed to redirect output.";
-		write(1,errRedirOut,strlen(errRedirOut));
-		write(1,"\n",1);
-		free(filenameSrc);
-		return 1;
-	}
-	*/
 	inFile = fopen(filenameSrc, "r");
 	if (inFile == NULL) {
 		char *errOpenInput = "Error! Failed to open input file.";
@@ -74,7 +52,6 @@ int main(int argc, char const *argv[])
 		free(filenameSrc);
 		return 1;
 	}
-	//printf("File opened\n");
 		
 	
 	do {
@@ -97,14 +74,13 @@ int main(int argc, char const *argv[])
 			write(1,"\n",1);
 			break;
 		}
-		large_token_buffer = str_filler (userInput, ";");
+		large_token_buffer = str_filler (userInput, "\n");
 		//printf("Reading lines...\n");
 		for (int i = 0; i < numLines; i++)
 		{
 			if (large_token_buffer.command_list[i] != NULL) {
 				small_token_buffer = str_filler (large_token_buffer.command_list[i], " ");
 				//pid_array[i] = fork();
-				//printf("Index pid array\n");
 				pid_array[i] = 0;
 				if (pid_array[i] < 0)
 				{
@@ -114,28 +90,23 @@ int main(int argc, char const *argv[])
 				}
 				if (pid_array[i] == 0)
 				{
-					//char *path = small_token_buffer.command_list[0];
-					//char * const arg[] = small_token_buffer.command_list + 1;
-					//printf("Read command\n");
 					if (execvp (small_token_buffer.command_list[0], (small_token_buffer.command_list)) == -1)
 					{
 						//error handling
 						perror("Execution Failed");
 						exit(1);
 					}
-					printf("A poor exit..\n");
-					exit(-1);
+					//printf("A poor exit..\n");
+					//exit(-1);
 				}
-			}		
-			free_command_line(&small_token_buffer);
-			memset (&small_token_buffer, 0, 0);
+			}
 		}
+		free_command_line(&small_token_buffer);
+		memset (&small_token_buffer, 0, 0);
 		free_command_line (&large_token_buffer);
 		memset (&large_token_buffer, 0, 0);
 		free (userInput);
 	} while(1);
 	free(filenameSrc);
-	//write(1,exitText,strlen(exitText));
-	//write(1,"\n",1);
 	return 0;
 }
