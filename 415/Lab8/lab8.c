fc57af60797d5ae5cd4c5017bd75bc2e49100cda
jdenckla <jdenckla@uoregon.edu>
2023-11-20 17:45:13 -0800

    Lab 8 progress

--- 415/Lab8/lab8.c
+++ 415/Lab8/lab8.c
@@ -9,13 +9,21 @@ pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_t thread_id[MAX_THREADS];    
 
+//personal additional, see ref: https://courses.engr.illinois.edu/cs241/fa2010/ppt/10-pthread-examples.pdf
+int *tidPtr;
+
 void * Print_thread(void* i)
 {
     // TODO: Allocate the mutex lock
+    pthread_mutex_lock(&mutex1);
     // TODO: print "First Print: " and the int i
+    printf("First Print: %d\n",i);
     // TODO: sleep for one second
+    sleep(1);
     // TODO: print "Second Print:" and the int i
+    printf("Second Print: %d\n",i);
     // TODO: Release the mutex lock
+    pthread_mutex_unlock(&mutex1);
     // TODO: You may also comment out the lock and unlock and see how the output changes
 
 }
@@ -33,6 +41,18 @@ int main(int argc, char * argv[])
     if(n > MAX_THREADS) n = MAX_THREADS;
 
     // TODO: Create n threads, each calling Print_thread
+    for (i = 0; i < n; i++) {
+        tidPtr = (int *)malloc(sizeof(int));
+        *tidPtr = i;
+        printf("Creating thread: %d\n",i);
+        //rc = pthread_create(&thread_id[i], NULL, Print_thread, );
+        rc = pthread_create(&thread_id[i], NULL, Print_thread, (void *)tidPtr);
+        if (rc) {
+            printf("Error - failed to create pthread: %d\n",rc);
+            exit(-1);
+        }
+    }
     // You may add print statements here as well if you want
+    return 0;
 
 }
